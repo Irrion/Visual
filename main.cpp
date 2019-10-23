bf74d39e7a2c6a3aed57c7974c281a99bef7e6d4
Irrion <mozhayko2017@mail.ru>
2019-10-23 23:30:51 +1000

    First commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int arr[4];
+	arr[0] = 5;
+	cout << arr[0] << endl;
+
+
+	int a, b, c;
+	
+	cin >> a;
+	cin >> b;
+	c = a + b;
+	cout << c << endl;
+
+
+	cout << "Пробный" << endl;
+	system("pause");
+}
